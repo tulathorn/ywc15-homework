282c1984f426b555c9f90483254147ed9192c5b1
tulathorn <tul.tulathorn@gmail.com>
2017-11-22 23:49:53 +0700

    Init front-end struc

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:5000/graphql/' }),
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client = {client}  >
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root'));
 registerServiceWorker();
